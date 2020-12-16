afc11d1ebbbb52292519934292676b2539753f3b
Sara Cemal <you@example.com>
2020-12-16 11:00:52 -0500

    commit tree 76979efb096c74c6676439ef3db6b2708ff61a4f

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
-// Add your code here
+// let submitData = {
+//     name: "sara",
+//     email: "saracemal@gmail.com"
+// }
+
+// let configObj = {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//       "Accept": "application/json"
+//     },
+//     body: JSON.stringify(submitData)
+//   };
+
+
+// fetch("http://localhost:3000/user", configObj)
+//   .then(function(response) {
+//     return response.json();
+//   })
+//   .then(function(object) {
+//     console.log(object);
+//   })
+//   .catch(function(error) {
+//     alert("Did not work!");
+//     console.log(error.message);
+//   });
+
+function submitData(name, email) {
+    return fetch( 'http://localhost:3000/users', {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify({
+          name,
+          email
+        })
+      })
+      .then( function ( response ) {
+        return response.json()
+      } )
+      .then( function ( object ) {
+        document.body.innerHTML = object[ "id" ]
+      } )
+      .catch( function ( error ) {
+        document.body.innerHTML = error.message
+      } )
+  }
